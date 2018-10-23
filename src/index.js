f2f0a4bf3b035ccfced9eaa918e32accc733a09a
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-23 15:48:15 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const meInReact = document.createElement('')
+const meInReact = document.createElement('div', {}, [title, paragraph, list]);
 
 
 ReactDOM.render(
