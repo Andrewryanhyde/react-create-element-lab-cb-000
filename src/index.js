cb751433d546ad5fc22cfb0139d98f338f1adfee
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-23 15:52:46 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const i1 = document.createElement('li', {}, "javascript")
+const i2 = document.createElement('li', {}, "react")
+const i3 = document.createElement('li', {}, "Movies")
+const i4 = document.createElement('li', {}, "Ice Cream")
+
 const title = document.createElement('h1', {}, "An Awesome Person");
 const paragraph = document.createElement('p', {}, "Who Is Learning React");
 const list = document.createElement('ul', { className: "my-interests", [i1, i2, i3, i4]});
