aabd95025a980217b08af7a3d6cfd451f8af13e2
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-23 15:49:16 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const title = document.createElement('h1', {}, "An Awesome Person");
+const paragrapg = document.createElement('p', {}, "Who Is Learning React");
 const meInReact = document.createElement('div', {}, [title, paragraph, list]);
 
 
